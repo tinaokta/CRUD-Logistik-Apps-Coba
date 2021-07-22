e246b75376917b3c0d8e72d3a01b9cab3d838c65
Tina Okta Riyanti <tina>
2021-07-22 21:16:15 +0700

    uas

---
+++ app/src/main/java/com/example/uastinaoktariyanti_crudapps/dashboard/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.uastinaoktariyanti_crudapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+    private NavController navController;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setPermission();
+
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        navController = Navigation.findNavController(this, R.id.nav_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+
+    private ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+        if (isGranted) {
+            return;
+        } else {
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission needed")
+                    .setMessage("Camera needed to get permission or QR Scanner cannot be used. App will close if you denied permission, are you sure want to continue?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            finish();
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            requestPermissionLauncher.launch(Manifest.permission.CAMERA);
+                        }
+                    })
+                    .create().show();
+        }
+    });
+
+    private void setPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.CAMERA) ==
+                PackageManager.PERMISSION_GRANTED) {
+            return;
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.CAMERA);
+        }
+    }
+}
